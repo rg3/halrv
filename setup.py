e71abfcd23a3c09a393db2c8e177f76564b95953
Ricardo Garcia <sarbalap+freshmeat@gmail.com>
2009-02-16 21:37:53 +0100

    Add a distutils setup.py script

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='halrv',
+	version='2',
+	description='HAL Removable Volumes Manager',
+	author='Ricardo Garcia',
+	url='http://github.com/rg3/halrv/tree/master',
+	data_files = [
+		('man/man1', ['halrv.1']),
+	],
+	scripts = [
+		'halrv',
+	],
+)
